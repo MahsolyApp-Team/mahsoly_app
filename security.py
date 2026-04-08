46ae386146f42fdc67c80a900c86d2747484d8d3
Ismail-72 <ah2196760@gmail.com>
2026-04-09 01:24:59 +0200

    Add files via upload

---
+++ security.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from dotenv import load_dotenv
+import os
+load_dotenv()
+SECRET_KEY = os.getenv("secret_key")
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE = int(os.getenv("access_token_expire", 60))
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(input_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(input_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
